b41e7889ab0fa0a7ca0e23157f788699df548590
Eva <evarooka@gmail.com>
2013-12-30 13:10:26 -0600

    Added more exercises from chap 2

--- fig02_01.C
+++ fig02_01.C
@@ -2,8 +2,36 @@
 //First Program in C
 #include <stdio.h>
 
-//Function Main begins program execution
+//Function main begins program execution
 int main(void)
 {
 	printf("Welcome to C!\n");
+
+
+//Printing on one line with 2 printf statements
+#include <stdio.h>
+//Function main begins program execution
+int main(void)
+
+	printf( "Welcome ");
+	printf( "to \n C!\n );
+
+/Fig. 2.5: fig02_05.c
+//Addition program.
+
+int integer1; //enter 1st number
+int integer2; //enter 2nd number
+int sum; //variable where sum is stored
+
+printf("Enter first integer/n" ); //prompt
+scanf("%d , &integer1 ); // read an integer
+
+printf("Enter second integer/n" ); //prompt
+scanf("%d , &integer2 ); // read an integer
+
+sum = integer1 + integer2; //assign total to sum
+
+printf( "Sum is %d\n" , sum ); // print sum
+
 } //end function main
+
